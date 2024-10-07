cd71f35de3d68b16ee930dbad9bde5986b33b390
Ihnatiev Serhii <17492722+thats-bot@users.noreply.github.com>
2024-10-07 18:58:44 +0200

    Add simple composable screen and viewmodel

---
+++ app/src/main/java/com/demo/communicationexample/presentation/BluetoothViewModel.kt
@@ -0,0 +1,40 @@
+package com.demo.communicationexample.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.demo.communicationexample.domain.BluetoothConnectionUseCase
+import com.demo.communicationexample.domain.entities.Message
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class BluetoothViewModel(
+    private val bluetoothConnectionUseCase: BluetoothConnectionUseCase
+) : ViewModel() {
+
+    private val _messages = MutableStateFlow<List<Message>>(emptyList())
+    val messages: StateFlow<List<Message>> = _messages
+
+    init {
+        viewModelScope.launch {
+            bluetoothConnectionUseCase.receiveMessage().collect { message ->
+                _messages.value = _messages.value + message
+            }
+        }
+    }
+
+    fun connect() {
+        viewModelScope.launch {
+            bluetoothConnectionUseCase.connect()
+        }
+    }
+
+    fun sendMessage(message: String) {
+        viewModelScope.launch {
+            val msg = Message(message, System.currentTimeMillis(), isSent = true)
+            bluetoothConnectionUseCase.sendMessage(msg)
+        }
+    }
+}
