7486b62e3ea201355ddff824db9828045301faae
Ihnatiev Serhii <17492722+thats-bot@users.noreply.github.com>
2024-10-08 07:46:24 +0200

    Add ConnectionResult to track operations success or failure

--- app/src/main/java/com/demo/communicationexample/domain/ConnectionResult.kt
+++ app/src/main/java/com/demo/communicationexample/domain/ConnectionResult.kt
@@ -4,7 +4,19 @@ package com.demo.communicationexample.domain
  * ConnectionResult is a sealed interface that represents the result of a connection attempt.
  */
 sealed interface ConnectionResult {
+
+    /**
+     * Represents a successful connection attempt
+     */
     data object Connected : ConnectionResult
+
+    /**
+     * Represents a message transfer attempt
+     */
     data class Transferred(val message: String) : ConnectionResult
+
+    /**
+     * Represents an error during the connection attempt
+     */
     data class Error(val message: String) : ConnectionResult
 }
