43f7b4e3c8c83cc578facd40230c8a5dcd23b110
Ihnatiev Serhii <17492722+thats-bot@users.noreply.github.com>
2024-10-08 10:25:42 +0200

    Fix permission issue for android 10

--- app/src/main/java/com/demo/communicationexample/presentation/MainActivity.kt
+++ app/src/main/java/com/demo/communicationexample/presentation/MainActivity.kt
@@ -1,22 +1,74 @@
 package com.demo.communicationexample.presentation
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import com.demo.communicationexample.communication.BluetoothManagerWrapper
 import com.demo.communicationexample.core.design.ConnectionTheme
-import com.demo.communicationexample.presentation.bt.BluetoothScreen
+import com.demo.communicationexample.presentation.bt.BluetoothApp
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @Inject
+    lateinit var managerWrapper: BluetoothManagerWrapper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        val enableBluetoothLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { /* Not needed */ }
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissionsMap ->
+            val canEnableBluetooth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                permissionsMap.values.all { it }
+            } else true
+
+            if (canEnableBluetooth && !managerWrapper.isBluetoothEnabled()) {
+                enableBluetoothLauncher.launch(
+                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                )
+            }
+        }
+
+        permissionLauncher.launch(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_ADVERTISE
+                )
+            }  else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+                arrayOf(
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.BLUETOOTH,
+                )
+            } else {
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN
+                )
+            }
+        )
+
         setContent {
             ConnectionTheme {
-                BluetoothScreen()
+                BluetoothApp()
             }
         }
     }
